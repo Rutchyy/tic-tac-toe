4f6c26c892140949586f6a26da4a3566dbf80728
Rutchy <171798882+Rutchyy@users.noreply.github.com>
2024-06-16 14:26:22 +1000

    Completed terminal functionality

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// This will store the basic data surrounding the game
 Gameboard = {
     board: ["0", "0", "0", "0", "0", "0", "0", "0", "0"],
     noughts: {name: "James"},
@@ -6,6 +7,7 @@ Gameboard = {
     result: null
 }
 
+// Prints the board to the console, will be decrepitated soon
 function logBoard() {
     console.log("-------------")
     for(let i = 0; i < Gameboard.board.length; i += 3) {
@@ -13,6 +15,7 @@ function logBoard() {
     }
 }
 
+// Updates the game board, if the position isn't occupied
 function updateBoard(index) {
     let updated;
     if(Gameboard.move == "noughts") {
@@ -22,27 +25,44 @@ function updateBoard(index) {
         updated = "C"
         Gameboard.move = "noughts"
     }
-    Gameboard.board.splice(index, 1, updated)
-}
-
-function check(x, y, z) {
-    if(x == y && x == z && x != "0") {
-        Gameboard.result = x == "N" ? "Noughts" : "Crosses"
+    if(Gameboard.board[index] == "0") {
+        Gameboard.board.splice(index, 1, updated)
+    } else {
+        console.log("That place is occupied, please enter another place!")
+        if(Gameboard.move == "noughts") {
+            Gameboard.move = "crosses"
+        } else {
+            Gameboard.move = "noughts"
+        }
     }
 }
 
+// Will find out if the position is won by either player
 function checkIfWon() {
-    for(let i = 0; i <= 3; i++) {
+    function check(x, y, z) {
+        if(x == y && x == z && x != "0") {
+            Gameboard.result = x == "N" ? "Noughts" : "Crosses"
+        }
+    }
+
+    for(let i = 0; i <= 2; i++) {
         check(Gameboard.board[i * 3], Gameboard.board[i * 3 + 1], Gameboard.board[i * 3 + 2])
         check(Gameboard.board[i], Gameboard.board[i + 3], Gameboard.board[i + 6])
         check(Gameboard.board[i], Gameboard.board[i + 4], Gameboard.board[i + 8])
         check(Gameboard.board[i + 2], Gameboard.board[i + 4], Gameboard.board[i + 6])
     }
 }
+
+function checkIfDrawn() {
+    // use a filter
+    // then do dom stuff
+}
+
 while(Gameboard.result == null) {
     updateBoard(prompt(`Input position for ${Gameboard.move}`))
     logBoard()
     checkIfWon()
+    checkIfDrawn()
 }
 
 console.log("The winner is: " + Gameboard.result)
